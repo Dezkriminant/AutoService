9c95743c222e246ca386277c022684b476e38556
vikto <viktorpiktor1605@gmail.com>
2026-04-29 21:44:35 +1000

    kaka

--- AutoService1/obj/Debug/net9.0/AutoService1.AssemblyInfo.cs
+++ AutoService1/obj/Debug/net9.0/AutoService1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoService1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8fc8869cfc3303f621513375430198f932b4731")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04e7eec31a185fa6f2356b5b34f5bce25e6f12dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoService1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoService1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
